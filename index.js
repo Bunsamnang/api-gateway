b4ded9d00a573590194d41e80d5b8db2eb8bcff2
Khut Bunsamnang <138737983+Bunsamnang@users.noreply.github.com>
2025-06-15 11:12:33 +0700

    add http:// to ep

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const proxy = httpProxy.createProxyServer();
 app.use("/user", authToken, authRole("user"), (req, res) => {
   console.log("INSIDE API GATEWAY USER ROUTE");
   proxy.web(req, res, {
-    target: "13.218.238.30:5001",
+    target: "http://13.218.238.30:5001",
     headers: {
       ...req.headers, // Forward all original headers
       "x-user-id": req.user._id, // Explicitly add user ID
@@ -27,19 +27,19 @@ app.use("/user", authToken, authRole("user"), (req, res) => {
 //REDIRECT TO THE ADMIN MICROSERVICE
 app.use("/admin", authToken, authRole("admin"), (req, res) => {
   console.log("INSIDE API GATEWAY ADMIN ROUTE");
-  proxy.web(req, res, { target: "13.218.238.30:5002" });
+  proxy.web(req, res, { target: "http://13.218.238.30:5002" });
 });
 
 app.use("/blog", (req, res) => {
   console.log("INSIDE API GATEWAY BLOG ROUTE");
-  proxy.web(req, res, { target: "3.83.112.120:5003" }); // or whatever port you use
+  proxy.web(req, res, { target: "http://3.83.112.120:5003" }); // or whatever port you use
 });
 
 //REDIRECT TO THE LOGIN(Authentication) MICROSERVICE
 app.use("/auth", (req, res) => {
   console.log("INSIDE API GATEWAY AUTH ROUTE");
 
-  proxy.web(req, res, { target: "54.89.139.190:5000" });
+  proxy.web(req, res, { target: "http://54.89.139.190:5000" });
 });
 
 app.listen(PORT, () => {
